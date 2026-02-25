01d2b4aab5a3f2bdf17188a06677415fc06625d1
Mahika Lakoul <mahikalakoul01@gmail.com>
2026-02-25 10:12:33 +0545

    Public game card

---
+++ src/components/OpenGameCard/OpenGameCard.jsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "./OpenGameCard.css";
+
+export default function OpenGameCard({
+  image,
+  name,
+  date,
+  time,
+  requiredPlayers,
+  phone,
+  chatLink = "/chat/temp-1",
+}) {
+  return (
+    <div className="openCard">
+      <img className="openCard__img" src={image} alt={name} />
+
+      <div className="openCard__body">
+        <h4 className="openCard__title">{name}</h4>
+
+        <div className="openCard__meta">
+          <div><b>Date:</b> {date}</div>
+          <div><b>Time:</b> {time}</div>
+          <div><b>Players Needed:</b> {requiredPlayers}</div>
+          <div><b>Phone:</b> {phone}</div>
+        </div>
+
+        <Link to={`/userRequest`} className="btn primary openCard__btn">
+          Join Game
+        </Link>
+      </div>
+    </div>
+  );
+}
