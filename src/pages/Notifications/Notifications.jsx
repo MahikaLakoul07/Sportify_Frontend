4869d4df3b8360f1e9028137e83896b2757300cf
Mahika Lakoul <mahikalakoul01@gmail.com>
2026-03-10 17:19:10 +0545

    notification

---
+++ src/pages/Notifications/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Notifications.css';
+
+export default function Notifications() {
+  return (
+    <div className="notifications-page">
+      <h1>Notifications</h1>
+      <p>Notifications.</p>
+    </div>
+  );
+}
